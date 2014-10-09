88ad6d6ec65fefe0d8a8c170a74e3060a52a2826
raul <raul@raul-VirtualBox>
2014-10-09 18:34:47 +0200

    First commit

---
+++ src/main/java/edu/upc/eetac/dsa/rgonzalez/Ejercicio6/Main.java
@@ -0,0 +1,28 @@
+package edu.upc.eetac.dsa.rgonzalez.Ejercicio6;
+
+
+
+public class Main {
+	public static void main(String[] args){
+		int img;
+		int text;
+		
+		
+		
+		
+		ArchivoTexto textfile = new ArchivoTexto();
+		ArchivoJpg imgfile = new ArchivoJpg();
+		
+		img=imgfile.cuentaBytes();
+		text=textfile.cuentaChars();
+		
+		if(img<text){
+			System.out.println("El archivo con más peso es registro.txt ");
+		}else{
+			System.out.println("El archivo con más peso es imagen.jpg ");
+		}
+		
+		
+	}
+
+}
